10d61c9852de3bc4922b663ca98a13874a29400d
robustonian <174159519+robustonian@users.noreply.github.com>
2025-12-16 22:50:35 +0900

    Add fuser command to kill existing process on port 7860

--- serve.sh
+++ serve.sh
@@ -1,6 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 export T5GEMMA_DETERMINISTIC=0
 export T5GEMMA_EMPTY_CACHE=0
+fuser -kvn tcp 7860
 uv run inference_gradio.py \
     --model_dir ../models/Aratako/T5Gemma-TTS-2b-2b \
     --port 7860 --max_batch 64 --max_segments 64 --inter_segment_silence 1.0
